f0efb53fc497a60670d53cc3499df556d8c97328
RoPalma95 <r.palma995@gmail.com>
2021-03-19 14:37:27 -0600

    added comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
 
   validates :username, uniqueness: true, length: { in: 4..12 }, presence: true
   validates :password, presence: true
